41c40686ee7f7dc4cd163ce2429f4a05097103f0
Rajabhayia <prayagrajsinghchauhan0123@gmail.com>
2025-02-05 20:12:02 +0530

    points functionality added

--- webpage/backend/backend/settings.py
+++ webpage/backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vxhf3!u1kw_kx5^ppa5-d$+jjfd3qwrl^tqk-i613)#@2mi7me
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -45,10 +45,6 @@ INSTALLED_APPS = [
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173',
-]
-
 
 
 MIDDLEWARE = [
